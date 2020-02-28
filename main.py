02bd6e2a33cbdce33e970fb4cb08a78d62b9d347
arhipovana2001 <55373084+arhipovana2001@users.noreply.github.com>
2020-02-28 22:07:29 +0700

    Update main.py
    На директорию ниже, существование файла, содержимое папки, содержимое папки выше

--- main.py
+++ main.py
@@ -12,5 +12,33 @@ def main():
     print('Путь:', path)
     path_up = path[:path.rfind('\\')]
     
+    def all_catalog(path, level=1):
+        print('Уровень =', level, 'Содержимое папки:', os.listdir(path))
+        for i in os.listdir(path):
+            if os.path.isdir(path + '\\' + i):
+                all_catalog(path + '\\' + i, level + 1)
+
+    def all_catalog_up(path, level=1):
+        print('Уровень =', level, 'Содержимое папки:', os.listdir(path))
+        for i in os.listdir(path):
+            if os.path.isdir(path + '\\' + i):
+                all_catalog(path + '\\' + i, level + 1)
+
+    def findFiles(name, path):
+        for i in os.listdir(path):
+            if i == name:
+                print(path + '\\' + i)
+            elif os.path.isdir(path + "\\" + i):
+                findFiles(name, path + "\\" + i)
+
+    def moveDown(path):
+        for i in os.listdir(path):
+            if os.path.isdir(i):
+                print(i)
+        print('Введите название папки:')
+        directory = str(input())
+        return all_catalog(directory, level=1)
+
+    
 
 main()
